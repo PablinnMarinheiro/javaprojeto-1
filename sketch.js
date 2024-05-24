438de82ea73bbedf16aea7eb6edad512a0ec1265
PablinnMarinheiro <00001088360610sp@al.educacao.sp.gov.br>
2024-05-24 14:55:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
+
 function setup() {
   createCanvas(800, 500);
-  background("rgb(255,255,255)");
+  background("red");
 }
 
 function draw() {
-  fill("black")
-  stroke("rgb(25,0,255)");
+  fill("rgb(0,0,0)")
+  stroke("rgb(6,17,100))");
   if (mouseIsPressed)
   ellipse(mouseX, mouseY,30, 50);
 }
